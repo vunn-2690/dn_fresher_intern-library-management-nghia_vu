e93d74884def1c9ac2ec7f67338b599f308f6ba2
vunn-2690 <nguyen.nam.vu@sun-asterisk.com>
2021-09-24 09:29:16 +0700

    Login/log out user
    login & logout
    
    Log in & Log out
    
    Remove folder vendor
    
    Add i18n
    
    fix indent and i18n Error
    
    fix indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   enum status: {member: 0, admin: 1}
+  has_secure_password
+
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create string, cost: cost
+  end
 end
