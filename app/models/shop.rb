b502a3f40c917e982fe59ccdcef643b19a0b336f
dnpnghia <idnpnlol@gmail.com>
2021-09-28 16:49:37 +0700

    List Shop
    List Shop
    
    Sign Up
    
    fix i18n for forgot password
    
    add i18n for fail reg
    
    Index Shop
    
    List Shops
    
    fix conflict
    
    fix end-line
    
    add style fix footer paginate
    
    fix name of search
    
    Refactor clean code

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,11 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_many :books, dependent: :destroy
+  scope :search_by_name, (lambda do |keyword|
+    where("name LIKE ?", "%#{keyword}%") if keyword.present?
+  end)
+
+  def all_books
+    self.books.recent_books
+  end
 end
