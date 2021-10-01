91dcfb021b5b284993d3c00c44ddc3680ede33b8
dnpnghia <idnpnlol@gmail.com>
2021-10-01 16:24:39 +0700

    store_location

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_save :downcase_email
   enum status: {member: 0, admin: 1}
   has_many :orders, dependent: :destroy
+  has_one :shop, dependent: :destroy
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :name, presence: true, length: {minimum: Settings.length.digit_10}
