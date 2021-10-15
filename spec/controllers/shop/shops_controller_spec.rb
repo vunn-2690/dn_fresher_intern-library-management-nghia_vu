9cff366237a3a91b6906431823672b287ac0a6e6
vunn-2690 <nguyen.nam.vu@sun-asterisk.com>
2021-10-15 14:18:25 +0700

    Rspec Shops Controller
    Rspec Controller Book
    
    Rspec Shops Controller
    
    shop-shop controller rspec
    
    rebase developVuNN
    
    changer name controller rspec
    
     fix endline
    
    fix comment

---
+++ spec/controllers/shop/shops_controller_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+include SessionsHelper
+
+RSpec.describe Shop::ShopsController, type: :controller do
+  describe "GET #show" do
+    before do
+      @user = FactoryBot.create :user
+      log_in @user
+    end
+    context "when shop does not exist" do
+      before do
+        get :show, params: {user_id: @user.id}
+      end
+      it "return flash danger" do
+        expect(flash[:danger]).to eq I18n.t("shops.not_found")
+      end
+      it "redirects to root_url" do
+        expect(response).to redirect_to root_url
+      end
+    end
+
+    context "when shop exist" do
+      let!(:shop){FactoryBot.create(:shop, user_id: @user.id)}
+      before do
+        get :show, params: {user_id: @user.id}
+      end
+      it "return all books" do
+        expect(assigns(:books)).to eq @user.shop.all_books
+      end
+    end
+
+    context "when invalid user" do
+      let!(:shop){FactoryBot.create(:shop, user_id: @user.id)}
+      before do
+        get :show, params: {user_id: 4}
+      end
+      it "return flash danger" do
+        expect(flash[:danger]).to eq I18n.t("shared.invalid_permision")
+      end
+      it "redirects to root_url" do
+        expect(response).to redirect_to root_url
+      end
+    end
+  end
+end
